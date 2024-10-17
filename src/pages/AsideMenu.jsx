b812f002276b96ddb89618b89b1dd33b01dc9307
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-10-17 15:31:39 +0200

    1.0

--- src/pages/AsideMenu.jsx
+++ src/pages/AsideMenu.jsx
@@ -2,30 +2,45 @@ import React from 'react';
 import avatarHead from '../assets/img/avatar-head.png';
 import avatarFull from '../assets/img/avatar-full.png'; 
 import '../css/AsideMenu.css';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 function AsideMenu() {
+
+  const location = useLocation();
+
   return (
-    <aside className='asideMenu'>
-      <div className='perfilContenedor'>
-        <div className='perfilImgContenedor'>
-          <img src={avatarHead} className='perfilImg front' alt="Perfil frontal"/>
-          <img src={avatarFull} className='perfilImg back' alt="Perfil trasera"/>
-        </div>
-        <h1>KarmelArtem</h1>
-      </div>
-      <nav>
-        <ul>
-          <li><NavLink to="/ilustracion" className={({ isActive }) => (isActive ? "active" : "")}>Ilustración</NavLink></li>
-          <li><NavLink to="/concept-art" className={({ isActive }) => (isActive ? "active" : "")}>Concept Art</NavLink></li>
-          <li><NavLink to="/retratos" className={({ isActive }) => (isActive ? "active" : "")}>Retratos</NavLink></li>
-          <li><NavLink to="/sketch" className={({ isActive }) => (isActive ? "active" : "")}>Sketch</NavLink></li>
-          <li><NavLink to="/sobre-mi" className={({ isActive }) => (isActive ? "active" : "")}>Sobre mí</NavLink></li>
-          <li><NavLink to="/contacto-social" className={({ isActive }) => (isActive ? "active" : "")}>Contacto/Social</NavLink></li>
-        </ul>
-      </nav>
-    </aside>
+    <>  
+        <aside className='asideMenu'>
+          <div className='perfilContenedor'>
+            <div className='perfilImgContenedor'>
+              <img src={avatarHead} className='perfilImg front' alt="Perfil frontal"/>
+              <img src={avatarFull} className='perfilImg back' alt="Perfil trasera"/>
+            </div>
+            <h1>KarmelArtem</h1>
+          </div>
+            <nav>
+            <ul>
+                <li>
+                <NavLink 
+                  to="/ilustracion" 
+                  className={({ isActive }) => 
+                    isActive || location.pathname === "/" ? "active" : ""
+                  }
+                >
+                  Ilustración
+                </NavLink>
+              </li>
+                <li><NavLink to="/conceptArt" activeClassName="active">Concept Art</NavLink></li>
+                <li><NavLink to="/retratos" activeClassName="active">Retratos</NavLink></li>
+                <li><NavLink to="/sketch" activeClassName="active">Sketch</NavLink></li>
+                <li><NavLink to="/sobre-mi" activeClassName="active">Sobre mí</NavLink></li>
+                <li><NavLink to="/contacto-social" activeClassName="active">Contacto/Social</NavLink></li>
+            </ul>
+            </nav>
+        </aside>
+    </>
   )
 }
 
 export default AsideMenu;
+
