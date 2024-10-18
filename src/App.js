6962b43974680856058b465761c0d2b220043889
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-10-18 08:22:23 +0200

    1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import AsideMenu from './components/AsideMenu';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Ilustracion from './components/Ilustracion';
 import ConceptArt from './components/ConceptArt';
 import Retratos from './components/Retratos';
@@ -8,20 +8,19 @@ import Sketch from './components/Sketch';
 import SobreMi from './components/SobreMi';
 import ContactoSocial from './components/ContactoSocial';
 
-
 function App() {
   return (
     <>
       <Router>
         <div className='app'>
-          <AsideMenu/>
+          <AsideMenu />
           <Routes>
-            <Route path="/conceptArt" element={<ConceptArt/>}/>
-            <Route path="/retratos" element={<Retratos/>}/>
-            <Route path="/sketch" element={<Sketch/>}/>
-            <Route path="/sobre-mi" element={<SobreMi/>}/>
-            <Route path="/contacto-social" element={<ContactoSocial/>}/>
-            <Route path="/" element={<Ilustracion/>}/>
+            <Route path="/conceptArt" element={<ConceptArt />} />
+            <Route path="/retratos" element={<Retratos />} />
+            <Route path="/sketch" element={<Sketch />} />
+            <Route path="/sobre-mi" element={<SobreMi />} />
+            <Route path="/contacto-social" element={<ContactoSocial />} />
+            <Route path="/" element={<Ilustracion />} />
           </Routes>
         </div>
       </Router>
